25d1d342ea64265f60db8fffdc7519657802f8c0
amalendu123 <220229@tkmce.ac.in>
2024-09-02 18:18:39 +0530

    ..

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,5 +1,8 @@
-import prisma from "../../../lib/prisma";
 
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 export default async function handler(req, res) {
   if (req.method == "POST") {
     try {
--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,5 +1,6 @@
-import prisma from "../../../lib/prisma";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export default async function handler(req, res) {
   if(req.method === 'POST') {
     try {
